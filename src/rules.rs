3685f3c011840588ff892c7c57259dd62c6f2477
Lab Rat <dev.frolics@gmail.com>
2024-04-22 03:19:09 +0530

    chore: add rules

---
+++ src/rules.rs
@@ -0,0 +1,15 @@
+mod a11_git_remote_http_ssh;
+mod a03_github_username;
+mod a09_commit_gpg_sign;
+mod a01_git_install;
+mod a02_git_name_email_setup;
+mod a05_github_credentials_http;
+mod a07_github_credentials_ssh;
+mod a06_gitlab_credentials_http;
+mod a08_gitlab_credentials_ssh;
+mod a04_gitlab_username;
+mod a12_no_secrets_staged;
+mod a13_git_lfs;
+mod a14_big_repo_size;
+mod a15_file_not_gitignored;
+
---
+++ src/rules/README.md
@@ -0,0 +1,5 @@
+## Naming Rules for "bgit rules"
+
+1. Don't use vague names
+2. Names should start with order `Cxx` where C is the character and xx are two-digit numbers.
+3. After `a99` is reached start alphabetically with `b01` and so on!
